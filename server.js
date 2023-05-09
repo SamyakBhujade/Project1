385db640e0c3d304babe58dd00f8fa828b4544dc
SamyakB <114329161+SamyakBhujade@users.noreply.github.com>
2023-05-09 20:18:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/hello', function (req, res) {
     res.send(person);
 });
 
-var server = app.listen(8888, function () {
+var server = app.listen(5555, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://localhost:8888", host, port)
+    console.log("Example app listening at http://localhost:5555", host, port)
 });
